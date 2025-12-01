aad39a25272c82dbeaa3b6702a0c7180d953c0fa
PhantHive <phantomhive72@yahoo.fr>
2025-12-01 16:02:56 +0100

    fix: app json

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -5,7 +5,7 @@
  */
 
 export const images = {
-    mascot: require('../../assets/images/mascot.png'),
+    mascot: require('../../assets/icon.png'),
     zumi: require('../../assets/images/zumi.png'),
     placeholder: require('../../assets/images/placeholder.jpg'),
 };
